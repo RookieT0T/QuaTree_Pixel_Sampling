c01182d1c75e99586348160cbda7c77438330cf6
Zequan_Tang <125717952+RookieT0T@users.noreply.github.com>
2024-06-25 10:09:11 +0800

    Add files via upload

--- quaTree.cpp
+++ quaTree.cpp
@@ -2,8 +2,40 @@
 #include <vector>
 #include <cstdlib>
 #include <cmath>
+#include <algorithm>
 using namespace std;
 
+// color
+class RGB {
+    public:
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+        RGB(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b){}
+
+};
+
+// supersampling
+class SUPER{
+    public:
+        bool on;
+        RGB color;
+        SUPER(bool signal, RGB rgb) : on(signal), color(rgb){}
+};
+
+// pixel
+class PIXEL{
+    public:
+        bool on;
+        SUPER s0;
+        SUPER s1;
+        SUPER s2;
+        SUPER s3;
+        PIXEL(bool signal, SUPER sam0, SUPER sam1, SUPER sam2, SUPER sam3) 
+        : on(signal), s0(sam0), s1(sam1), s2(sam2), s3(sam3){}
+};
+
+// * mag^2
 int calArea(vector<vector<int>> tri){
     vector<int> A = tri[0];
     vector<int> B = tri[1];
@@ -35,20 +67,25 @@ bool intersect(vector<vector<int>> triEdge, vector<vector<int>> sqEdge){
 }
 
 // check the specified square overlapps with the triangle
-bool overlap(vector<vector<int>> tri, int rowS, int rowE, int colS, int colE){
+bool overlap(vector<vector<int>> triOld, float rowP, float colP, float space){
+    // scale the triangle by 2
+    vector<vector<int>> tri(triOld);
+    for(auto &e : tri){
+        for(auto &n : e){
+            n *= 2;
+        }
+    }
     // triangle area
     int triArea = calArea(tri);
-
     // triangle edges
-     vector<vector<vector<int>>> triEdges = {{tri[0], tri[1]}, {tri[1], tri[2]}, {tri[2], tri[0]}};
+    vector<vector<vector<int>>> triEdges = {{tri[0], tri[1]}, {tri[1], tri[2]}, {tri[2], tri[0]}};
 
     // square coordinates
-    vector<int> topLeft = {colS, rowS};
-    vector<int> topRight = {colE + 1, rowS};
-    vector<int> botLeft = {colS, rowE + 1};
-    vector<int> botRight = {colE + 1, rowE + 1};
+    vector<int> topLeft = {int(colP * 2), int(rowP * 2)};
+    vector<int> topRight = {int((colP + space) * 2), int(rowP * 2)};
+    vector<int> botLeft = {int(colP * 2), int((rowP + space) * 2)};
+    vector<int> botRight = {int((colP + space) * 2), int((rowP + space) * 2)};
     vector<vector<int>> square = {topLeft, topRight, botLeft, botRight};
-
     // square egdes
     vector<vector<int>> left = {botLeft, topLeft};
     vector<vector<int>> upper = {topLeft, topRight};
@@ -71,7 +108,7 @@ bool overlap(vector<vector<int>> tri, int rowS, int rowE, int colS, int colE){
 
     // S1: check if triangle vertices are within the square
     for(auto i : tri){
-        if(i[0] >= colS && i[0] <= colE + 1 && i[1] >= rowS && i[1] <= rowE + 1){
+        if(i[0] >= colP && i[0] <= colP + 1 && i[1] >= rowP && i[1] <= rowP + 1){
             return true;
         }
     }
@@ -88,7 +125,8 @@ bool overlap(vector<vector<int>> tri, int rowS, int rowE, int colS, int colE){
     return false;
 }
 
-void traverse(vector<vector<bool>> &pixels, int rowS, int rowE, int colS, int colE, vector<vector<int>> &tri){
+/*
+void traverseRe(vector<vector<bool>> &pixels, int rowS, int rowE, int colS, int colE, vector<vector<int>> &tri){
     if(rowS > rowE || colS > colE){
         return;
     }
@@ -104,42 +142,81 @@ void traverse(vector<vector<bool>> &pixels, int rowS, int rowE, int colS, int co
 
     int rowMid = rowS + (rowE -  rowS) / 2;
     int colMid = colS + (colE - colS) / 2;
-    traverse(pixels, rowS, rowMid, colS, colMid, tri);      // top left
-    traverse(pixels, rowS, rowMid, colMid + 1, colE, tri);  // top right
-    traverse(pixels, rowMid + 1, rowE, colS, colMid, tri);  // bottom left
-    traverse(pixels, rowMid + 1, rowE, colMid + 1, colE, tri);  // bottom right
+    traverseRe(pixels, rowS, rowMid, colS, colMid, tri);      // top left
+    traverseRe(pixels, rowS, rowMid, colMid + 1, colE, tri);  // top right
+    traverseRe(pixels, rowMid + 1, rowE, colS, colMid, tri);  // bottom left
+    traverseRe(pixels, rowMid + 1, rowE, colMid + 1, colE, tri);  // bottom right
+}
+*/
+
+void traverseTi(vector<vector<PIXEL>> &pixels, int rowS, int rowE, int colS, int colE, vector<vector<int>> &tri){
+    for(int i = rowS; i < rowE; ++i){
+        for(int k = colS; k < colE; ++k){
+            if(overlap(tri, i, k, 1.0)){
+                pixels[i][k].on = true;
+
+                // check four super samplings
+                // first super sampling block
+                if(overlap(tri, i, k, 0.5)){
+                    pixels[i][k].s0.on = true;
+                }
+                // second super sampling block
+                if(overlap(tri, i, k + 0.5, 0.5)){
+                    pixels[i][k].s1.on = true;
+                }
+                // third super sampling block
+                if(overlap(tri, i + 0.5, k, 0.5)){
+                    pixels[i][k].s2.on = true;
+                }
+                // forth super sampling block
+                if(overlap(tri, i + 0.5, k + 0.5, 0.5)){
+                    pixels[i][k].s3.on = true;
+                }
+            }
+        }
+    }
 }
 
 int main(){
-    int times = 10;
-    int len = 10 * times;
-    int wid = 7 * times;
-    vector<vector<bool>> pixels(wid, vector<bool>(len, false));
 
-    // triangle coordinates
-    vector<vector<int>> tri0 = {{3 * times, 2 * times}, {5 * times, 0 * times}, {5 * times, 4 * times}};
-    vector<vector<int>> tri1 = {{5 * times, 4 * times}, {7 * times, 2 * times}, {9 * times, 4 * times}};
-    vector<vector<int>> tri2 = {{5 * times, 4 * times}, {7 * times, 6 * times}, {5 * times, 7 * times}}; 
-    vector<vector<int>> tri3 = {{1 * times, 4 * times}, {5 * times, 4 * times}, {3 * times, 6 * times}}; 
-    vector<vector<vector<int>>> tri = {tri0, tri1, tri2, tri3};
+    int times = 5;
+    int len = 10 * times;
+    int wid = 10 * times;
+    vector<vector<PIXEL>> pixels(wid, vector<PIXEL>(len, PIXEL(false, SUPER(false, RGB(0, 0, 0)), SUPER(false, RGB(0, 0, 0)), SUPER(false, RGB(0, 0, 0)), SUPER(false, RGB(0, 0, 0)))));
+
+    // triangle coordinates & attributes
+    vector<vector<int>> tri0 = {{0 * times, 0 * times}, {8 * times, 0 * times}, {0 * times, 8 * times}};
+    vector<vector<vector<int>>> tri = {tri0};
+    RGB v1_color = {255, 0, 0};
+    RGB v2_color = {0, 255, 0};
+    RGB v3_color = {0, 0, 255};
+    
+    // call rasterization process
     for(auto t : tri){
-        traverse(pixels, 0, wid - 1, 0, len - 1, t);
+        int boxRowS = min({t[0][1], t[1][1], t[2][1]});
+        int boxRowE = max({t[0][1], t[1][1], t[2][1]});
+        int boxColS = min({t[0][0], t[1][0], t[2][0]});
+        int boxColE = max({t[0][0], t[1][0], t[2][0]});
+        //traverseRe(pixels, 0, wid - 1, 0, len - 1, t);
+        traverseTi(pixels, boxRowS, boxRowE, boxColS, boxColE, t);
     }
     
-    // horizontal axis
+    // show all chosen pixles
+    /*
     cout << "  ";
     for(int i = 0; i < len; ++i){
         cout << i << " ";
     }
     cout << endl;
-    // vertical axis
-    // traverse
-    int temp = 0;
+    */
+    
+    // vertical axis & traverse
+    //int temp = 0;
     for(auto i : pixels){
-        cout << temp << " ";
-        temp++;
+        //cout << temp << " ";
+        //temp++;
         for(auto k : i){
-            cout << k << " ";
+            cout << k.on << " ";
         }
         cout << endl;
     }
