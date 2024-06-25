219a035c4c6cf9c335b46f99b06819e97ca062fc
Zequan_Tang <125717952+RookieT0T@users.noreply.github.com>
2024-06-26 07:22:03 +0800

    Add files via upload

--- quaTree.cpp
+++ quaTree.cpp
@@ -2,17 +2,19 @@
 #include <vector>
 #include <cstdlib>
 #include <cmath>
+#include <tuple>
 #include <algorithm>
+#include <iomanip>
+#include <map>
 using namespace std;
 
 // color
 class RGB {
     public:
-        uint8_t red;
-        uint8_t green;
-        uint8_t blue;
-        RGB(uint8_t r, uint8_t g, uint8_t b) : red(r), green(g), blue(b){}
-
+        float red;
+        float green;
+        float blue;
+        RGB(float r, float g, float b) : red(r), green(g), blue(b){}
 };
 
 // supersampling
@@ -35,7 +37,16 @@ class PIXEL{
         : on(signal), s0(sam0), s1(sam1), s2(sam2), s3(sam3){}
 };
 
-// * mag^2
+// triangle vertex & its color attributes
+class TRIVER{
+    public:
+        int x;
+        int y;
+        RGB color;
+        TRIVER(int xcoord, int ycoord, RGB col)
+        : x(xcoord), y(ycoord), color(col){}
+};
+
 int calArea(vector<vector<int>> tri){
     vector<int> A = tri[0];
     vector<int> B = tri[1];
@@ -67,9 +78,9 @@ bool intersect(vector<vector<int>> triEdge, vector<vector<int>> sqEdge){
 }
 
 // check the specified square overlapps with the triangle
-bool overlap(vector<vector<int>> triOld, float rowP, float colP, float space){
+bool overlap(vector<TRIVER> triOld, float rowP, float colP, float space){
+    vector<vector<int>> tri = {{triOld[0].x, triOld[0].y}, {triOld[1].x, triOld[1].y}, {triOld[2].x, triOld[2].y}};
     // scale the triangle by 2
-    vector<vector<int>> tri(triOld);
     for(auto &e : tri){
         for(auto &n : e){
             n *= 2;
@@ -121,56 +132,113 @@ bool overlap(vector<vector<int>> triOld, float rowP, float colP, float space){
             }
         }
     }
-
     return false;
 }
 
-/*
-void traverseRe(vector<vector<bool>> &pixels, int rowS, int rowE, int colS, int colE, vector<vector<int>> &tri){
-    if(rowS > rowE || colS > colE){
-        return;
+tuple<float, float, float> interpolate(vector<TRIVER> &triOld, float rowP, float colP, map<tuple<int, int, int>, tuple<float, float, float>> &cache){
+    vector<vector<int>> tri = {{triOld[0].x, triOld[0].y}, {triOld[1].x, triOld[1].y}, {triOld[2].x, triOld[2].y}};
+    // scale the triangle by 2
+    for(auto &e : tri){
+        for(auto &n : e){
+            n *= 2;
+        }
     }
-    // base case 1
-    if(rowS == rowE && colS == colE){
-        pixels[rowS][colS] = overlap(tri, rowS, rowE, colS, colE) || pixels[rowS][colS];
-        return;
-    }
-    // base case 2
-    if(overlap(tri, rowS, rowE, colS, colE) == false){
-        return;
-    }
+    // triangle area
+    float triArea = (float)calArea(tri);
 
-    int rowMid = rowS + (rowE -  rowS) / 2;
-    int colMid = colS + (colE - colS) / 2;
-    traverseRe(pixels, rowS, rowMid, colS, colMid, tri);      // top left
-    traverseRe(pixels, rowS, rowMid, colMid + 1, colE, tri);  // top right
-    traverseRe(pixels, rowMid + 1, rowE, colS, colMid, tri);  // bottom left
-    traverseRe(pixels, rowMid + 1, rowE, colMid + 1, colE, tri);  // bottom right
+    // sub-triangle PBC
+    int newRowP = int(rowP * 2);
+    int newColP = int(colP * 2);
+    vector<int> temp0 = tri[0];
+    tri[0] = {newColP, newRowP};
+    float areaPBC = (float)calArea(tri);
+    tri[0] = temp0;
+
+    // sub-triangle PAC
+    vector<int> temp1 = tri[1];
+    tri[1] = {newColP, newRowP};
+    float areaPAC = (float)calArea(tri);
+    tri[1] = temp1;
+
+    // sub-triangle PAB
+    vector<int> temp2 = tri[2];
+    tri[2] = {newColP, newRowP};
+    float areaPAB = (float)calArea(tri);
+    tri[2] = temp2;
+
+    // triOld[0].color -> color of vertex A
+    // triOld[1].color -> color of vertex B
+    // triOld[2].color -> color of vertex C
+    int threshold1 = 10;
+    int threshold2 = 100;
+    float cPBC = areaPBC / triArea;
+    int key1 = round(cPBC * threshold1);
+    float cPAC = areaPAC / triArea;
+    int key2 = round(cPAC * threshold1);
+    float cPAB = areaPAB / triArea;
+    int key3 = round(cPAB * threshold1);
+
+    auto key = make_tuple(key1, key2, key3);
+    if(cache.find(key) != cache.end()){
+        return cache[key];
+    }
+    else{
+        float red, green, blue = 0;
+        red = cPBC * triOld[0].color.red + cPAC * triOld[1].color.red + cPAB * triOld[2].color.red;
+        red = round(red * threshold2) / threshold2; // precision - two digits behind the decimal point
+
+        green = cPBC * triOld[0].color.green + cPAC * triOld[1].color.green + cPAB * triOld[2].color.green;
+        green = round(green * threshold2) / threshold2;
+
+        blue = cPBC * triOld[0].color.blue + cPAC * triOld[1].color.blue + cPAB * triOld[2].color.blue;
+        blue = round(blue * threshold2) / threshold2;
+        
+        auto ans = make_tuple(red, green, blue);
+        cache[key] = ans;
+        return ans;
+    }
 }
-*/
 
-void traverseTi(vector<vector<PIXEL>> &pixels, int rowS, int rowE, int colS, int colE, vector<vector<int>> &tri){
+void traverseTi(vector<vector<PIXEL>> &pixels, int rowS, int rowE, int colS, int colE, vector<TRIVER> &tri){
+    // hashmap
+    map<tuple<int, int, int>, tuple<float, float, float>> approximate_cache;
     for(int i = rowS; i < rowE; ++i){
         for(int k = colS; k < colE; ++k){
             if(overlap(tri, i, k, 1.0)){
                 pixels[i][k].on = true;
 
-                // check four super samplings
+                // check four super samplings and update their color if they should intersect the triangle
                 // first super sampling block
                 if(overlap(tri, i, k, 0.5)){
                     pixels[i][k].s0.on = true;
+                    auto ans0 = interpolate(tri, i, k, approximate_cache);
+                    pixels[i][k].s0.color.red = get<0>(ans0);
+                    pixels[i][k].s0.color.green = get<1>(ans0);
+                    pixels[i][k].s0.color.blue = get<2>(ans0);
                 }
                 // second super sampling block
                 if(overlap(tri, i, k + 0.5, 0.5)){
                     pixels[i][k].s1.on = true;
+                    auto ans1 = interpolate(tri, i, k + 0.5, approximate_cache);
+                    pixels[i][k].s1.color.red = get<0>(ans1);
+                    pixels[i][k].s1.color.green = get<1>(ans1);
+                    pixels[i][k].s1.color.blue = get<2>(ans1);
                 }
                 // third super sampling block
                 if(overlap(tri, i + 0.5, k, 0.5)){
                     pixels[i][k].s2.on = true;
+                    auto ans2 = interpolate(tri, i + 0.5, k, approximate_cache);
+                    pixels[i][k].s2.color.red = get<0>(ans2);
+                    pixels[i][k].s2.color.green = get<1>(ans2);
+                    pixels[i][k].s2.color.blue = get<2>(ans2);
                 }
                 // forth super sampling block
                 if(overlap(tri, i + 0.5, k + 0.5, 0.5)){
                     pixels[i][k].s3.on = true;
+                    auto ans3 = interpolate(tri, i + 0.5, k + 0.5, approximate_cache);
+                    pixels[i][k].s3.color.red = get<0>(ans3);
+                    pixels[i][k].s3.color.green = get<1>(ans3);
+                    pixels[i][k].s3.color.blue = get<2>(ans3);
                 }
             }
         }
@@ -178,47 +246,36 @@ void traverseTi(vector<vector<PIXEL>> &pixels, int rowS, int rowE, int colS, int
 }
 
 int main(){
-
-    int times = 5;
+    int times = 10;
     int len = 10 * times;
     int wid = 10 * times;
     vector<vector<PIXEL>> pixels(wid, vector<PIXEL>(len, PIXEL(false, SUPER(false, RGB(0, 0, 0)), SUPER(false, RGB(0, 0, 0)), SUPER(false, RGB(0, 0, 0)), SUPER(false, RGB(0, 0, 0)))));
 
     // triangle coordinates & attributes
-    vector<vector<int>> tri0 = {{0 * times, 0 * times}, {8 * times, 0 * times}, {0 * times, 8 * times}};
-    vector<vector<vector<int>>> tri = {tri0};
-    RGB v1_color = {255, 0, 0};
-    RGB v2_color = {0, 255, 0};
-    RGB v3_color = {0, 0, 255};
+    vector<TRIVER> tri = {TRIVER(1 * times, 1 * times, RGB(1, 0, 0)), TRIVER(8 * times, 2 * times, RGB(1.0, 1.0, 0)), TRIVER(0 * times, 8 * times, RGB(0, 0, 1))};
     
-    // call rasterization process
-    for(auto t : tri){
-        int boxRowS = min({t[0][1], t[1][1], t[2][1]});
-        int boxRowE = max({t[0][1], t[1][1], t[2][1]});
-        int boxColS = min({t[0][0], t[1][0], t[2][0]});
-        int boxColE = max({t[0][0], t[1][0], t[2][0]});
-        //traverseRe(pixels, 0, wid - 1, 0, len - 1, t);
-        traverseTi(pixels, boxRowS, boxRowE, boxColS, boxColE, t);
-    }
-    
-    // show all chosen pixles
-    /*
-    cout << "  ";
-    for(int i = 0; i < len; ++i){
-        cout << i << " ";
-    }
-    cout << endl;
-    */
-    
-    // vertical axis & traverse
-    //int temp = 0;
-    for(auto i : pixels){
-        //cout << temp << " ";
-        //temp++;
-        for(auto k : i){
-            cout << k.on << " ";
+    // use bounding box and then call rasterization + interpolation process
+    int boxRowS = min({tri[0].y, tri[1].y, tri[2].y});
+    int boxRowE = max({tri[0].y, tri[1].y, tri[2].y});
+    int boxColS = min({tri[0].x, tri[1].x, tri[2].x});
+    int boxColE = max({tri[0].x, tri[1].x, tri[2].x});
+    traverseTi(pixels, boxRowS, boxRowE, boxColS, boxColE, tri);
+
+    // need code to visualize each sampling point of each pixel in vector named pixels
+    for(int i = 0; i < pixels.size(); ++i){
+        for(int times = 0; times < 2; ++times){
+            for (int j = 0; j < pixels[i].size(); ++j){
+                if(times == 0){
+                    cout << setprecision(2) << "(" << pixels[i][j].s0.color.red << ", " << pixels[i][j].s0.color.green << ", " << pixels[i][j].s0.color.blue << ")  ";
+                    cout << "(" << pixels[i][j].s1.color.red << ", " << pixels[i][j].s1.color.green << ", " << pixels[i][j].s1.color.blue << ")  ";
+                }
+                else{
+                    cout << "(" << pixels[i][j].s2.color.red << ", " << pixels[i][j].s2.color.green << ", " << pixels[i][j].s2.color.blue << ")  ";
+                    cout << "(" << pixels[i][j].s3.color.red << ", " << pixels[i][j].s3.color.green << ", " << pixels[i][j].s3.color.blue << ")  "; 
+                }
+            }
+            cout << endl;
         }
-        cout << endl;
     }
     return 0;
 }
